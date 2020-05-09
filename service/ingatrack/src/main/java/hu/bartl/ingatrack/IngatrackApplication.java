5edd2845e92c8e6513a2d223360158b7e30621a5
bartlbalazs <balazs.bartl@aliz.ai>
2020-05-09 14:34:57 +0200

    Initial commit

---
+++ service/ingatrack/src/main/java/hu/bartl/ingatrack/IngatrackApplication.java
@@ -0,0 +1,13 @@
+package hu.bartl.ingatrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngatrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngatrackApplication.class, args);
+	}
+
+}
---
+++ service/ingatrack/src/test/java/hu/bartl/ingatrack/IngatrackApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.bartl.ingatrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngatrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
