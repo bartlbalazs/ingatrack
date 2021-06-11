54670db2a66cd8e26baf4412e7d9e5d03d57830c
Balázs Bartl <bartlbalazs@gmail.com>
2021-06-11 13:52:02 +0200

    Intermittent scheduled tracking

--- service/ingatrack-api/src/main/java/hu/bartl/ingatrack/service/ScheduledService.java
+++ service/ingatrack-api/src/main/java/hu/bartl/ingatrack/service/ScheduledService.java
@@ -1,19 +1,24 @@
 package hu.bartl.ingatrack.service;
 
+import com.google.common.base.Stopwatch;
 import hu.bartl.ingatrack.config.ApplicationConfig;
 import hu.bartl.ingatrack.exception.NotFoundException;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @AllArgsConstructor
 @Slf4j
 public class ScheduledService {
+
+    private static final int PERIOD_LENGTH_IN_MINUTES = 8;
 
     private final ApplicationConfig applicationConfig;
     private final SubscriptionService subscriptionService;
@@ -32,13 +37,30 @@ public class ScheduledService {
     @Scheduled(cron = "0 0 3 * * *")
     @Async
     public void doJob() {
+        log.info("Starting subscription tracking on schedule.");
+        var stopwatch = Stopwatch.createStarted();
         log.info("Tracking properties by property subscription");
         subscriptionService.listPropertySubscriptions()
                 .forEach(s -> trackingService.trackProperty(s.getPropertyId(), s.getId()));
 
+        subscriptionService.listPropertySubscriptions()
+                .forEach(s -> doIntermittentTracking(() -> trackingService.trackProperty(s.getPropertyId(), s.getId()), PERIOD_LENGTH_IN_MINUTES, stopwatch));
+
         log.info("Tracking properties by search subscription");
         subscriptionService.listSearchSubscriptions()
-                .forEach(s -> trackingService.trackSearch(s.getQuery(), s.getId()));
+                .forEach(s -> doIntermittentTracking(() -> trackingService.trackSearch(s.getQuery(), s.getId()), PERIOD_LENGTH_IN_MINUTES, stopwatch));
+        log.info("Scheduled subscription tracking finished.");
+    }
+
+    @SneakyThrows
+    private void doIntermittentTracking(Runnable trackingMethod, int periodLengthInMinutes, Stopwatch stopwatch) {
+        if (stopwatch.elapsed(TimeUnit.MINUTES) > periodLengthInMinutes) {
+            log.info("Maximum consecutive work time exceeded. Wait for " + periodLengthInMinutes + " minutes.");
+            Thread.sleep(periodLengthInMinutes * 1000 * 60);
+            stopwatch.reset();
+            stopwatch.start();
+        }
+        trackingMethod.run();
     }
 
     @Async
