7d10cef5820ff3fd1a22051cff894bd8fd249e0a
Balázs Bartl <bartlbalazs@gmail.com>
2021-06-11 16:16:31 +0200

    TrackingDataService

---
+++ service/ingatrack-api/src/main/java/hu/bartl/ingatrack/service/TrackingDataService.java
@@ -0,0 +1,65 @@
+package hu.bartl.ingatrack.service;
+
+import com.google.api.client.util.Lists;
+import hu.bartl.ingatrack.component.DateProvider;
+import hu.bartl.ingatrack.component.HtmlPageParser;
+import hu.bartl.ingatrack.config.ApplicationConfig;
+import hu.bartl.ingatrack.entity.TrackingData;
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.springframework.http.HttpStatus;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.jsoup.Connection.Method.GET;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class TrackingDataService {
+
+    private final ApplicationConfig applicationConfig;
+    private final HtmlPageParser htmlPageParser;
+    private final DateProvider dateProvider;
+
+    @SneakyThrows
+    @Retryable(value = org.jsoup.HttpStatusException.class, maxAttempts = 5, backoff = @Backoff(delay = 1000 * 180, multiplier = 2))
+    public TrackingData fetchTrackingData(long propertyId) {
+        log.info("Fetching tracking data for property with ID: " + propertyId);
+        TimeUnit.MILLISECONDS.sleep(applicationConfig.getRequestDelayMs());
+        var htmlPage = Jsoup.connect(applicationConfig.getDatasourceUrl() + "/" + propertyId).method(GET).ignoreHttpErrors(true).execute();
+        if (htmlPage.statusCode() == HttpStatus.OK.value()) {
+            String html = new String(htmlPage.bodyAsBytes(), UTF_8.name());
+            return htmlPageParser.parsePropertyPage(propertyId, html);
+        } else {
+            return TrackingData.create(propertyId, dateProvider.now());
+        }
+    }
+
+    @SneakyThrows
+    @Retryable(value = org.jsoup.HttpStatusException.class, maxAttempts = 5, backoff = @Backoff(delay = 1000 * 180, multiplier = 2))
+    public List<Long> fetchPropertyIds(String query) {
+        log.info("Fetching property IDs from query: " + query);
+        TimeUnit.MILLISECONDS.sleep(applicationConfig.getRequestDelayMs());
+        List<Long> result = Lists.newArrayList();
+
+        var htmlPage = Jsoup.connect(applicationConfig.getDatasourceUrl() + "/lista/" + query + "?page=1").method(GET).execute();
+        var page = htmlPageParser.parseSearchPage(new String(htmlPage.bodyAsBytes(), UTF_8.name()));
+        result.addAll(page.getProperties());
+
+        while (page.hasNext()) {
+            htmlPage = Jsoup.connect(applicationConfig.getDatasourceUrl() + "/lista/" + query + "?page=" + page.getNextPage()).method(GET).execute();
+            page = htmlPageParser.parseSearchPage(new String(htmlPage.bodyAsBytes(), UTF_8.name()));
+            result.addAll(page.getProperties());
+        }
+
+        return result;
+    }
+}
--- service/ingatrack-api/src/main/java/hu/bartl/ingatrack/service/TrackingService.java
+++ service/ingatrack-api/src/main/java/hu/bartl/ingatrack/service/TrackingService.java
@@ -1,26 +1,12 @@
 package hu.bartl.ingatrack.service;
 
-import com.google.api.client.util.Lists;
-import hu.bartl.ingatrack.component.DateProvider;
-import hu.bartl.ingatrack.component.HtmlPageParser;
-import hu.bartl.ingatrack.config.ApplicationConfig;
-import hu.bartl.ingatrack.entity.TrackingData;
 import hu.bartl.ingatrack.repository.TrackingDataRepository;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.jsoup.Jsoup;
-import org.springframework.http.HttpStatus;
-import org.springframework.retry.annotation.Backoff;
-import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
-import static org.jsoup.Connection.Method.GET;
 
 @Service
 @AllArgsConstructor
@@ -28,61 +14,24 @@ import static org.jsoup.Connection.Method.GET;
 public class TrackingService {
 
     private final TrackingDataRepository trackingDataRepository;
-    private final ApplicationConfig applicationConfig;
-    private final HtmlPageParser htmlPageParser;
-    private final DateProvider dateProvider;
-
+    private final TrackingDataService trackingDataService;
 
     @SneakyThrows
     public void trackProperty(long propertyId, String requestSource) {
         log.info("Tracking property " + propertyId + " initiated by subscription: " + requestSource);
-        var trackingData = fetchTrackingData(propertyId);
+        var trackingData = trackingDataService.fetchTrackingData(propertyId);
         trackingData.setInsertedBy(requestSource);
         trackingDataRepository.save(trackingData);
     }
 
     public void trackSearch(String query, String requestSource) {
         log.info("Tracking query " + query + " initiated by subscription: " + requestSource);
-        var trackingData = fetchPropertyIds(query)
+        var trackingData = trackingDataService.fetchPropertyIds(query)
                 .stream()
-                .map(this::fetchTrackingData)
+                .map(trackingDataService::fetchTrackingData)
                 .map(t -> t.toBuilder().insertedBy(requestSource).build())
                 .collect(Collectors.toList());
         trackingDataRepository.save(trackingData);
         log.info("Tracking query " + query + " finished.");
-    }
-
-    @SneakyThrows
-    @Retryable(value = org.jsoup.HttpStatusException.class, maxAttempts = 5, backoff = @Backoff(delay = 1000 * 180, multiplier = 2))
-    public TrackingData fetchTrackingData(long propertyId) {
-        log.info("Fetching tracking data for property with ID: " + propertyId);
-        TimeUnit.MILLISECONDS.sleep(applicationConfig.getRequestDelayMs());
-        var htmlPage = Jsoup.connect(applicationConfig.getDatasourceUrl() + "/" + propertyId).method(GET).ignoreHttpErrors(true).execute();
-        if (htmlPage.statusCode() == HttpStatus.OK.value()) {
-            String html = new String(htmlPage.bodyAsBytes(), UTF_8.name());
-            return htmlPageParser.parsePropertyPage(propertyId, html);
-        } else {
-            return TrackingData.create(propertyId, dateProvider.now());
-        }
-    }
-
-    @SneakyThrows
-    @Retryable(value = org.jsoup.HttpStatusException.class, maxAttempts = 5, backoff = @Backoff(delay = 1000 * 180, multiplier = 2))
-    public List<Long> fetchPropertyIds(String query) {
-        log.info("Fetching property IDs from query: " + query);
-        TimeUnit.MILLISECONDS.sleep(applicationConfig.getRequestDelayMs());
-        List<Long> result = Lists.newArrayList();
-
-        var htmlPage = Jsoup.connect(applicationConfig.getDatasourceUrl() + "/lista/" + query + "?page=1").method(GET).execute();
-        var page = htmlPageParser.parseSearchPage(new String(htmlPage.bodyAsBytes(), UTF_8.name()));
-        result.addAll(page.getProperties());
-
-        while (page.hasNext()) {
-            htmlPage = Jsoup.connect(applicationConfig.getDatasourceUrl() + "/lista/" + query + "?page=" + page.getNextPage()).method(GET).execute();
-            page = htmlPageParser.parseSearchPage(new String(htmlPage.bodyAsBytes(), UTF_8.name()));
-            result.addAll(page.getProperties());
-        }
-
-        return result;
     }
 }
